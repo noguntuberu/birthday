56c99f1952a9cdfd063587596011f7dd22ded291
ifeanyi chimbo <ifykenchi@gmail.com>
2025-03-21 18:47:49 +0100

    Updated frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Members from "./components/members";
+import Members from "./features/MembersArea/components/members";
 // import Search from "./components/members/search";
-import Explore from "./components/members/explore";
-import Sidebar from "./components/sidebar";
-import Profile from "./components/members/profile/profile";
-import RegisterForm from "./Register/components/RegisterForm";
+import Explore from "./features/MembersArea/components/members/explore";
+import Sidebar from "./features/MembersArea/components/sidebar";
+import Profile from "./features/MembersArea/components/members/profile/profile";
+import RegisterForm from "./features/Register/components/RegisterForm";
 import "./app.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="members-container">
-        <Sidebar />
-        <div className="members-wrapper">
-          <Routes>
-            <Route path="/" element={<Members />} />
-            {/* <Route path="/user-search" element={<Search />} /> */}
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='members-container'>
+				<Sidebar />
+				<div className='members-wrapper'>
+					<Routes>
+						<Route path='/' element={<Members />} />
+						{/* <Route path="/user-search" element={<Search />} /> */}
+						<Route path='/explore' element={<Explore />} />
+						<Route path='/profile' element={<Profile />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
