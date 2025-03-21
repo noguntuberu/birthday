c02973def9b6546fefbf9daaa9e8241b16ec40c0
ifeanyi chimbo <ifykenchi@gmail.com>
2025-03-21 20:13:41 +0100

    updated structure

---
+++ frontend/src/features/MembersArea/members/profile/profile.tsx
@@ -0,0 +1,11 @@
+import "./profile.css";
+import ProfilePage from "../../../../pages/profile";
+
+export default function Profile() {
+	return (
+		<div>
+			<h1>My Profile</h1>
+			<ProfilePage />
+		</div>
+	);
+}
